ed539e3fcdbbc9d62348c827737bc0ab81867c9e
Victor "Nate" Graf <nategraf1@gmail.com>
2019-02-17 18:17:41 -0600

    fix Hash getall

--- trol/collection.py
+++ trol/collection.py
@@ -1492,7 +1492,7 @@ class Hash(Collection, collections.MutableMapping):
 
         :rtype: dict
         """
-        return dict([(k, self.deservialize(v)) for (k, v) in self.redis.hgetall(self.key)])
+        return dict([(k, self.deserialize(v)) for (k, v) in self.redis.hgetall(self.key)])
 
     def hvals(self):
         """
